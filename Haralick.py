6d1ac63d4055975b4338faa3edf6c913fe135152
kuzzmak <kuzminski.antonio@gmail.com>
2020-03-12 00:24:10 +0100

    update

--- Haralick.py
+++ Haralick.py
@@ -4,27 +4,59 @@ class HaralickFeatures:
 
     def __init__(self, glcm):
         self.glcm = glcm
-        self.num_level, _, _, _ = self.glcm.shape
-        self.I, self.J = np.ogrid[0:self.num_level, 0:self.num_level]
 
-    def contrast(self):
-        weights = (self.I - self.J) ** 2
-        weights = weights.reshape((self.num_level, self.num_level, 1, 1))
-        results = np.apply_over_axes(np.sum, (self.glcm * weights), axes=(0, 1))[0, 0, 0]
+
+
+
+    def greycoprops(self, prop='contrast'):
+        """
+            Funkcija za izračunavanje Haralickovih funkcija
+
+            f1 -> energy +
+            f2 -> contrast +
+            f3 -> correlation
+            f4 -> sum of squares: variance
+            f5 -> inverse difference moment
+            f6 -> sum average
+
+        :param prop: funkcija koju je potrebno izračunati
+        :return: vrijednost funkcije koja se izračunava
+        """
+
+
+        (num_level, num_level2, num_dist, num_angle) = self.glcm.shape
+        if num_level != num_level2:
+            raise ValueError('num_level and num_level2 must be equal.')
+        if num_dist <= 0:
+            raise ValueError('num_dist must be positive.')
+        if num_angle <= 0:
+            raise ValueError('num_angle must be positive.')
+
+        # normalize each GLCM
+        self.glcm.shape = self.glcm.shape.astype(np.float64)
+        glcm_sums = np.apply_over_axes(np.sum, self.glcm.shape, axes=(0, 1))
+        glcm_sums[glcm_sums == 0] = 1
+        self.glcm.shape /= glcm_sums
+
+        # create weights for specified property
+        I, J = np.ogrid[0:num_level, 0:num_level]
+        if prop == 'contrast':
+            weights = (I - J) ** 2
+        elif prop == 'homogeneity':
+            weights = 1. / (1. + (I - J) ** 2)
+        elif prop in ['energy', 'entropy']:
+            pass
+        else:
+            raise ValueError('%s is an invalid property' % prop)
+
+        # compute property for each GLCM
+        if prop == 'energy':
+            results = np.apply_over_axes(np.sum, (self.glcm.shape ** 2), axes=(0, 1))[0, 0]
+        elif prop == 'entropy':
+            results = np.apply_over_axes(np.sum, self.glcm.shape, axes=(0, 1))[0, 0]
+        elif prop in ['contrast', 'homogeneity']:
+            weights = weights.reshape((num_level, num_level, 1, 1))
+            results = np.apply_over_axes(np.sum, (self.glcm.shape * weights), axes=(0, 1))[0, 0]
+
         return results
-
-    def energy(self):
-        results = np.apply_over_axes(np.sum, (self.glcm ** 2), axes=(0, 1))[0, 0, 0]
-        return results
-
-    def homogeneity(self):
-        weights = 1. / (1. + (self.I - self.J) ** 2)
-        weights = weights.reshape((self.num_level, self.num_level, 1, 1))
-        results = np.apply_over_axes(np.sum, (self.glcm * weights), axes=(0, 1))[0, 0, 0]
-        return results
-
-    def entropy(self):
-        results = np.apply_over_axes(np.sum, self.glcm, axes=(0, 1))[0, 0, 0]
-        return results
-
 
--- test.py
+++ test.py
@@ -1,30 +1,30 @@
 import numpy as np
 import cv2 as cv
-from matplotlib import pyplot as plt
-from skimage.feature import local_binary_pattern
-
-imagePath = r"/home/tonkec/PycharmProjects/Crowd-density-estimation-based-on-LBPCM/data/processedData/21.jpg"
-
-# imagePath = r"/home/tonkec/Desktop/220px-Lenna_(test_image).png"
-
-img = cv.imread(imagePath, cv.IMREAD_GRAYSCALE)
-
-sobelx = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=3)
-sobely = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=3)
-
-# sobel1 = np.power(sobelx, 2) + np.power(sobely, 2)
-sobel1 = np.abs(sobelx) / 2 + np.abs(sobely) / 2
-# sobel1 = np.sqrt(sobel1)
-
-
-sobel = cv.Sobel(img, cv.CV_8U, 1, 1, ksize=3)
-
-lbp = local_binary_pattern(sobel1, 8, 1, method='default')
-print(np.amin(lbp))
-print(np.amax(lbp))
-print(lbp)
-cv.imshow("pic", lbp.astype('uint8'))
-cv.waitKey(0)
+# from matplotlib import pyplot as plt
+# from skimage.feature import local_binary_pattern
+#
+# imagePath = r"/home/tonkec/PycharmProjects/Crowd-density-estimation-based-on-LBPCM/data/processedData/21.jpg"
+#
+# # imagePath = r"/home/tonkec/Desktop/220px-Lenna_(test_image).png"
+#
+# img = cv.imread(imagePath, cv.IMREAD_GRAYSCALE)
+#
+# sobelx = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=3)
+# sobely = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=3)
+#
+# # sobel1 = np.power(sobelx, 2) + np.power(sobely, 2)
+# sobel1 = np.abs(sobelx) / 2 + np.abs(sobely) / 2
+# # sobel1 = np.sqrt(sobel1)
+#
+#
+# sobel = cv.Sobel(img, cv.CV_8U, 1, 1, ksize=3)
+#
+# lbp = local_binary_pattern(sobel1, 8, 1, method='default')
+# print(np.amin(lbp))
+# print(np.amax(lbp))
+# print(lbp)
+# cv.imshow("pic", lbp.astype('uint8'))
+# cv.waitKey(0)
 
 
 # import tkinter
@@ -79,3 +79,103 @@ cv.waitKey(0)
 # button.pack(side=tkinter.BOTTOM)
 #
 # tkinter.mainloop()
+
+# a = np.array([[1, 2, 3], [1, 1, 1], [0, 4, 2]])
+# print(a)
+#
+# print()
+# column_sum = np.sum(a, axis=0)
+# row_sum = np.sum(a, axis=1)
+
+# meanx = np.sum(row_sum) / len(row_sum)
+# print(str(meanx))
+
+# b = np.apply_over_axes(np.sum, (a ** 2), axes=(0, 1))
+#
+# print(b)
+from skimage.feature import greycomatrix
+from math import radians
+import math
+
+
+filename = r"/home/tonkec/PycharmProjects/Crowd-density-estimation-based-on-LBPCM/data/processedData/21.jpg"
+
+image = cv.imread(filename, cv.IMREAD_GRAYSCALE)
+
+print(image.shape)
+
+cell = image[0:64, 0:64]
+
+distances = [1]
+angles = [0, radians(90)]
+
+dimension = len(distances) + len(angles)
+
+glcm = greycomatrix(image.astype(int), distances, angles, levels=256, normed=True)
+
+sums_x = []
+sums_y = []
+
+for d in range(len(distances)):
+    for a in range(len(angles)):
+        sums_x.append(np.sum(np.sum(glcm[:, :, d, a], axis=0)))
+        sums_y.append(np.sum(np.sum(glcm[:, :, d, a], axis=1)))
+
+mean_x = [x / 256 for x in sums_x]
+mean_y = [y / 256 for y in sums_y]
+
+sigma_x = [0] * dimension
+sigma_y = [0] * dimension
+
+sigma_x = [math.sqrt(1 / (256 - 1) * x) for x in sigma_x]
+sigma_y = [math.sqrt(1 / (256 - 1) * y) for y in sigma_y]
+
+k = 0
+
+f3 = [0] * dimension
+
+for d in range(len(distances)):
+    for a in range(len(angles)):
+        for i in range(256):
+            for j in range(256):
+
+                sigma_x[k] += (glcm[i][j][d][a] - mean_x[k]) ** 2
+                sigma_y[k] += (glcm[i][j][d][a] - mean_y[k]) ** 2
+                f3[k] += (i * j * glcm[i][i][d][a] - mean_x[k] * mean_y[k]) / (sigma_x[k] * sigma_y[k])
+
+        k += 1
+
+print(f3)
+#
+# print(np.sum(np.sum(glcm[:, :, 0, 0], axis=0)))
+# print(np.sum(np.sum(glcm[:, :, 1, 0], axis=0)))
+# print(np.sum(glcm, axis=(0, 1)))
+
+
+
+# print(np.shape(glcm[:, :, :, 0]))
+# print(glcm)
+# column = np.sum(glcm, axis=(0, 1, 2)) # po stupcu
+# row = np.sum(glcm, axis=(0, 1, 3)) # po redu
+
+
+
+# a = np.array([[[[1, 2, 3],
+#                [2, 2, 2],
+#                [3, 3, 3]],
+#
+#               [[1, 1, 1],
+#                [0, 0, 0],
+#                [5, 3, 1]]],
+#
+#               [[[1, 2, 3],
+#                 [2, 2, 2],
+#                 [3, 3, 3]],
+#
+#                [[1, 1, 1],
+#                 [0, 0, 0],
+#                 [5, 3, 1]]]])
+#
+# column = np.sum(a, axis=(0, 1, 2)) # po stupcu
+# row = np.sum(a, axis=(0, 1, 3)) # po redu
+# print(row)
