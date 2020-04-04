10e3741afffb65157e46ea49a010e2a341ef6b62
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-04 17:34:07 +0200

    updated frame for future classification

--- Pages/CLP2.py
+++ Pages/CLP2.py
@@ -4,32 +4,47 @@ from PIL import Image, ImageTk
 
 import util
 from Pages import FVC2Page as fvcP2
-from Pages import InitializationPage as iP
+from Pages import PictureClassificationFrame as pcp
 
 class CLP2(tk.Frame):
-
-    # class PictureClassificationPanel(tk.Frame):
-    #
-    #     def __init__(self, parent, controller):
-    #         tk.Frame.__init__(self, parent)
-    #         self.grid_columnconfigure(0, weight=1)
-    #
 
     def __init__(self, parent, controller):
 
         tk.Frame.__init__(self, parent)
         self.grid_columnconfigure(0, weight=1)
 
-        self.numberOfModels = controller.frames[fvcP2.FVC2Page].numberOfModels.get()
+        self.pcpFrames = []
 
-        self.labelModelName = tk.Label(self, text="SVM - gray - 0.8%")
-        self.labelModelName.pack(padx=10, pady=10)
+        self.middleFrame = tk.Frame(self)
+        self.middleFrame.pack(padx=10, pady=10)
 
-        blank = cv.imread(controller.app.configuration['blankImagePath'])
-        blank = util.resizePercent(blank, 40)
-        self.blank = ImageTk.PhotoImage(image=Image.fromarray(blank))
-        self.labelImage = tk.Label(self, image=self.blank)
-        self.labelImage.pack(padx=10, pady=10)
+        _pcp = pcp.PictureClassificationPanel(self.middleFrame, controller)
+        _pcp.pack(side="left", padx=10, pady=10)
+        self.pcpFrames.append(_pcp)
 
-        buttonBack = tk.Button(self, text="Back", command=lambda: controller.show_frame(iP.InitializationPage))
+        buttonBack = tk.Button(self, text="Back", command=lambda: controller.show_frame(fvcP2.FVC2Page))
         buttonBack.pack(side="bottom", padx=5, pady=5)
+
+    def updateClassificationFrame(self, controller):
+
+        if controller.frames[fvcP2.FVC2Page].numberOfModels.get() == 2:
+
+            if len(self.pcpFrames) == 2:
+                pass
+            else:
+                _pcp = pcp.PictureClassificationPanel(self.middleFrame, controller)
+                _pcp.pack(side="left", padx=10, pady=10)
+                self.pcpFrames.append(_pcp)
+        else:
+
+            self.middleFrame.destroy()
+            self.middleFrame = tk.Frame(self)
+            self.middleFrame.pack(padx=10, pady=10)
+
+            _pcp = pcp.PictureClassificationPanel(self.middleFrame, controller)
+            _pcp.pack(side="left", padx=10, pady=10)
+
+            self.pcpFrames = [_pcp]
+
+        for m in controller.app.selectedModels:
+            print(m)
