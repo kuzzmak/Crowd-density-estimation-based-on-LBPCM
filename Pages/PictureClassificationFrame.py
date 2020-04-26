b62decd43d0c6ac5f20054cd70e6bccf4fa4cc04
kuzzmak <kuzminski.antonio@gmail.com>
2020-04-26 21:28:24 +0200

    bug fix

--- Pages/PictureClassificationFrame.py
+++ Pages/PictureClassificationFrame.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import cv2 as cv
 from PIL import ImageTk, Image
-
+import os
 import util
 
 """
@@ -21,8 +21,8 @@ class PictureClassificationPanel(tk.Frame):
         self.labelModelName.pack(padx=10, pady=10)
 
         # bijela pozadina slike kad još nije ništa učitano
-        blank = cv.imread(controller.app.configuration['iconsDirectory'] + '/blankImage.jpg')
-        blank = util.resizePercent(blank, 40)
+        blank = cv.imread(os.path.join(controller.app.configuration['iconsDirectory'], 'blankImage.jpg'))
+        blank = util.resizePercent(blank, 20)
         self.blank = ImageTk.PhotoImage(image=Image.fromarray(blank))
         self.labelImage = tk.Label(self, image=self.blank)
         self.labelImage.pack(padx=10, pady=10)
